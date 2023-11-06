01ab70a80943eadf8ea75463551986d7676fffe7
3dintersekt <3dintersekt>
2023-11-06 17:57:14 -0500

    Update padding

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,7 @@ import Image from 'next/image'
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+    <main className="flex min-h-screen flex-col items-center pt-8 p-16 justify-evenly">
       {/* <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
         <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
           Get started by editing&nbsp;
@@ -40,21 +40,12 @@ export default function Home() {
       </div>
       
       <div className="relative place-items-center text-xl text-center">
-        <p className="mb-1">
+        <p className="mb-2">
           Website Under Construction
         </p>
         <p>
           Email: <a href='mailto:foobar@example.com'>foobar@example.com</a>
         </p>
-      </div>
-
-      <div>
-      </div>
-
-      <div>
-      </div>
-
-      <div>
       </div>
 
       {/* <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
