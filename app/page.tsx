81f90f029d17cffed3d20535993ff8fea350b827
David Germiquet <david@trulycanadian.net>
2024-01-27 15:10:01 -0500

    Fix location of phone and add 1

--- app/page.tsx
+++ app/page.tsx
@@ -18,7 +18,7 @@ export default function Home() {
       
       <div className="relative place-items-center text-xl text-center">
         <p>
-          <a className="contact"  href ="tel:+4167975778">T<span className="orange">|</span> 416-797-5778 </a> <br/>
+          <a className="contact"  href ="tel:+14167975778">T<span className="orange">|</span> 1-416-797-5778 </a> <br/>
           <a className="contact"  href='mailto:info@3dintersekt.com'>E<span className="orange">|</span> info@3Dintersekt.com</a>
         </p>
       </div>
